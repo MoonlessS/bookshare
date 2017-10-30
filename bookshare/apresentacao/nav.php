683622d69cc9963ee6570509c0219a163e2aa759
FilipaNunes <33135941+FilipaNunes@users.noreply.github.com>
2017-10-30 23:25:02 +0000

    Add files via upload

--- bookshare/apresentacao/nav.php
+++ bookshare/apresentacao/nav.php
@@ -7,7 +7,7 @@ echo "
         <li><a href='home/'>Home</a></li>
         <li><a href='ongoing-books-list/'>Ongoing Books List</a></li>
         <li><a href='finished-books-list/'>Finished Books List</a></li>
-        <li><a href='write-a-book'>Write A Book</a></li>
+        <li><a href='login'>Login</a></li>
     </ul>
 </nav>
 ";
