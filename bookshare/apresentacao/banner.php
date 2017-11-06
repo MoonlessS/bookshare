2400351a4e1aeaaf7e93f239bf64ea38a16afa4b
Filipa Nunes <filipanunes12@hotmail.com>
2017-11-06 14:40:10 +0000

    Pesquisa de livros, capitulos e geral

--- bookshare/apresentacao/banner.php
+++ bookshare/apresentacao/banner.php
@@ -5,7 +5,7 @@
         <div class='banner'>
 			
 			<div class='advanced_search'>
-				<form action='pesquisa/search.php' method='get'>
+				<form action='pesquisa/resultados.php' method='get'>
 					<input class='advanced_search' type='text' name='general_search' placeholder='Search'>
 				</form>	
 				<a href='pesquisa/index.php'><input class='advanced_search' type='image' src='img/search.jpg'></a>
