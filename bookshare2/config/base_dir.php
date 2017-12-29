a1080ea25d957e7aa4775ac14ae8d3de2096b2db
moonlesss <ped.gomes_pg@hotmail.com>
2017-12-29 22:10:05 +0000

    base_dir - filipa

--- bookshare2/config/base_dir.php
+++ bookshare2/config/base_dir.php
@@ -1,6 +1,7 @@
 <?php
-$BASE_DIR = '/srv/www/htdocs/bookshare/bookshare2/';
-$BASE_URL = 'http://localhost/bookshare/bookshare2/';
+$BASE_DIR = '/usr/users2/miec2013/up201305298/public_html/trabalhosSiem/trabalhoPHP-final/bookshare2/bookshare/';
+$BASE_URL = 'https://paginas.fe.up.pt/~up201305298/trabalhosSiem/trabalhoPHP-final/bookshare2/bookshare/';
+
 
  ?>
 <?php
@@ -8,6 +9,7 @@ $BASE_URL = 'http://localhost/bookshare/bookshare2/';
 $BASE_DIR = '/usr/users2/miec2013/up201305298/public_html/trabalhosSiem/trabalhoPHP-final/bookshare2/bookshare/';
 $BASE_URL = 'https://paginas.fe.up.pt/~up201305298/trabalhosSiem/trabalhoPHP-final/bookshare2/bookshare/';
 
-
+$BASE_DIR = '/srv/www/htdocs/bookshare/bookshare2/';
+$BASE_URL = 'http://localhost/bookshare/bookshare2/';
 */
  ?>
