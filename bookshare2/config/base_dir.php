4391695b1509f516ff5dcf7ccb35cc7920181fb4
moonlesss <ped.gomes_pg@hotmail.com>
2017-12-29 23:23:47 +0000

    Merge branch 'master' of https://github.com/MoonlessS/bookshare

--- bookshare2/config/base_dir.php
+++ bookshare2/config/base_dir.php
@@ -2,6 +2,7 @@
 
 $BASE_DIR = '/usr/users2/miec2013/up201305298/public_html/trabalhosSiem/trabalhoPHP-final/bookshare/bookshare2/';
 $BASE_URL = 'https://paginas.fe.up.pt/~up201305298/trabalhosSiem/trabalhoPHP-final/bookshare/bookshare2/';
+
  ?>
 
 <?php
