e11410261dee7de4c25ac0bea77f4787ad247b69
FilipaNunes <filipanunes12@hotmail.com>
2017-12-29 23:05:45 +0000

    Alteração do base_dir

--- bookshare2/config/base_dir.php
+++ bookshare2/config/base_dir.php
@@ -1,6 +1,6 @@
 <?php
-$BASE_DIR = '/usr/users2/miec2013/up201305298/public_html/trabalhosSiem/trabalhoPHP-final/bookshare2/bookshare/';
-$BASE_URL = 'https://paginas.fe.up.pt/~up201305298/trabalhosSiem/trabalhoPHP-final/bookshare2/bookshare/';
+$BASE_DIR = '/usr/users2/miec2013/up201305298/public_html/trabalhosSiem/trabalhoPHP-final/bookshare/bookshare2/';
+$BASE_URL = 'https://paginas.fe.up.pt/~up201305298/trabalhosSiem/trabalhoPHP-final/bookshare/bookshare2/';
 
 
  ?>
